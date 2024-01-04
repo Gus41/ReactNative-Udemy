393ac13ffa1e9fc42e1a07ce2d3206d97e8a9d38
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-01-04 15:25:01 -0300

    Create Button.tsx

---
+++ Exe/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React  from "react";
+import { Button, Text, View } from "react-native";
+import styles from "../styles";
+
+export default ()=>{
+    return(
+        <Button title="Press"
+        
+        />
+    )
+}
