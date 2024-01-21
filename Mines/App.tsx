ee55df5c8192f0a337ca68bc60ab95f4f0cb9bc5
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-01-21 13:41:25 -0300

    .

--- Mines/App.tsx
+++ Mines/App.tsx
@@ -1,11 +1,17 @@
 import { Component } from "react";
 import { SafeAreaView } from "react-native";
 import params from "./src/params";
+import Field from "./src/components/Field";
 export default class App extends Component{
   render(){
     return(
       <SafeAreaView>
-
+        <Field/>
+        <Field opened nearMines = {3}/>
+        <Field/>
+        <Field opened nearMines = {1}/>
+        <Field opened nearMines = {2}/>
+        <Field opened nearMines = {5}/>
       </SafeAreaView>
     )
   }
---
+++ Mines/src/components/Field.js
@@ -0,0 +1,69 @@
+import React from "react";
+import params from "../params";
+import { StyleSheet, View, Text } from "react-native";
+
+
+export default (props)=>{
+    
+    const {mined , opened, nearMines} = props
+
+    //mined = Field está minado
+    //opened = Field aberto
+    //Nearmines = Quantas minas arredor desse field
+
+    const styleField = [styles.field]
+    if(opened){
+        styleField.push(styles.opened)
+    }
+    if(styleField.length === 1){
+        styleField.push(styles.regular)
+    }
+
+
+    let color = null
+    if(nearMines > 0){
+        if(nearMines === 1){
+            color = '#2A2807'
+        }else if(nearMines === 2){
+            color = '#2B520F'
+        }else if(nearMines > 2 && nearMines < 6 ){
+            color = '#F9060A'
+        }else{
+            color = '#F221A9'
+        }
+    }
+
+    return(
+        <View style={styleField}>
+            {!mined && opened && nearMines > 0 ? 
+            <Text style={[styles.label,{color:color}]}>
+                {nearMines}
+            </Text>: false}
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    field:{
+        height:params.blockSize,
+        width:params.blockSize,
+        borderWidth:params.borderSize
+
+    },
+    regular:{
+        backgroundColor:'#999',
+        borderLeftColor:'#CCC',
+        borderTopColor:'#CCC',
+        borderRightColor:'#333',
+        borderBottomColor:'#333'
+    },
+    opened:{
+        backgroundColor:'#999',
+        borderColor:'#777',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    label:{
+        fontWeight:'bold',
+        fontSize:params.fontSize
+    }
+})
