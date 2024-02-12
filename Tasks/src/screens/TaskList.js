d971dc6357cbe9ea9ba352f31bd5c1fe69c6d0fb
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-02-12 16:00:52 -0300

    .

---
+++ Tasks/src/screens/TaskList.js
@@ -0,0 +1,41 @@
+import { ImageBackground, StyleSheet, Text, View } from "react-native"
+import React from "react"
+import { Component } from "react"
+import todayImage from '../../assets/imgs/today.jpg'
+import moment from "moment"
+import "moment/locale/pt-br"
+export default class TaskList extends Component{
+    render(){
+        const today = moment().locale('pt-br').format('ddd,D [de] MMMM')
+        return(
+            <View style={styles.container}>
+                <ImageBackground
+                style={styles.background} 
+                source={todayImage}>
+                    <View style={styles.tittleBar}>
+                        <Text>Hoje</Text>
+                        <Text>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskContainer}>
+                    <Text>Task List</Text>
+                </View>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    },
+    background:{
+        flex:3
+    },
+    taskContainer:{
+        flex:7
+    },
+    tittleBar:{
+        flex:1,
+        justifyContent:'flex-end'
+    }
+})
