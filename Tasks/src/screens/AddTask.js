04fa825ac35d38209c460cd594b058ddbee4ee78
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-02-15 09:28:24 -0300

    Update AddTask.js

--- Tasks/src/screens/AddTask.js
+++ Tasks/src/screens/AddTask.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Component } from "react";
-import { Modal, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { Modal, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import commomStyles from "../commomStyles";
 
-
+const initialState = { desc : '' }
 export default class AddTask extends Component{
-
+    state = {
+        ...initialState
+    }
     render(){
         return(
             <Modal transparent={true} visible={this.props.isVisible}
@@ -17,6 +19,20 @@ export default class AddTask extends Component{
                 </TouchableWithoutFeedback>
                 <View style={styles.container}>
                     <Text style={styles.header}>Nova Task</Text>
+                    <TextInput  style={styles.input} 
+                    placeholder="Informe a task"
+                    onChangeText={desc=> this.setState({desc})}
+                    value={this.state.desc}
+                    
+                    />
+                    <View style={styles.buttons}>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <TouchableWithoutFeedback 
                 onPress={this.props.onCancel}>
@@ -34,7 +50,6 @@ const styles = StyleSheet.create({
     },
     container:{
         backgroundColor:"white",
-        flex:1,
 
     },
     header:{
@@ -44,5 +59,23 @@ const styles = StyleSheet.create({
         textAlign:"center",
         padding:10,
         fontSize:18
+    },
+    buttons:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    input:{
+        height:40,
+        margin:15,
+        fontFamily:commomStyles.fontFamily,
+        backgroundColor:'white',
+        borderWidth:1,
+        borderColor:'#E3E3E3',
+        borderRadius:5
+    },
+    button:{
+        margin:20,
+        marginRight:30,
+        color:commomStyles.color.todayColor
     }
 })
