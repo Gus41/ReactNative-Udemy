39e969d342258c4b4339918284c764d3f564db2d
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-03-17 17:44:35 -0300

    .

--- Drink/src/contexts/UserContext.tsx
+++ Drink/src/contexts/UserContext.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from "react";
 import IUser from "../interfaces/UserInterface";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 
 interface IUserContext{
     user: IUser,
@@ -22,6 +24,25 @@ interface UserProviderProps {
 export const UserProvider: React.FC<UserProviderProps> = ({children})=>{
     const [goal,setGoal] = useState<number>(GOAL)
     const [user] = useState<IUser>(USER)
+    
+    const storeData = async (value:number)=>{
+        try{
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('state',jsonValue)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    const getData: ()=>Promise<Number> = async ()=>{
+        try{
+            const jsonValue = await AsyncStorage.getItem('state')
+            return jsonValue != null ? JSON.parse(jsonValue) : null
+        }catch(e){
+            console.log(e)
+        }
+    }
+
     return(
         <UserContext.Provider value={{goal,setGoal,user}}>
             {children}
