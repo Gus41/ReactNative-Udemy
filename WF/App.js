33e52b3f84238f7b6c4e72d1199ed15a8a451f42
Gustavo <123191793+Gus41@users.noreply.github.com>
2024-05-07 11:11:34 -0300

    .

--- WF/App.js
+++ WF/App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Routes from './src/routes/Routes';
 
 export default function App() {
@@ -8,11 +6,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
